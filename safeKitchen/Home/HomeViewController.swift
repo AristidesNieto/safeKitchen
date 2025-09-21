c59c3666333c7f8609164fdcf5dd5b3fda5be6f1
Luis Serrano <luis.zempo15@gmail.com>
2025-09-20 20:34:39 -0600

    fisrt homeui

---
+++ safeKitchen/Home/HomeViewController.swift
@@ -0,0 +1,22 @@
+//
+//  HomeViewController.swift
+//  safeKitchen
+//
+//  Created by Luis Angel Zempoalteca on 20/09/25.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+     @IBOutlet weak var Header: UILabel!
+     
+     
+
+}

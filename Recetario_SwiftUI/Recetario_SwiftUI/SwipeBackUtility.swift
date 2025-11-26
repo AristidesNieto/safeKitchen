bd96a4ec04f76c58cda49cf2a17484d806e901a3
Luis Serrano <luis.zempo15@gmail.com>
2025-11-25 22:44:17 -0600

    aggg

---
+++ Recetario_SwiftUI/Recetario_SwiftUI/SwipeBackUtility.swift
@@ -0,0 +1,20 @@
+//
+//  SwipeBackUtility.swift
+//  Recetario_SwiftUI
+//
+//  Created by Luis Angel Zempoalteca on 25/11/25.
+//
+
+import SwiftUI
+
+extension UINavigationController: @retroactive UIGestureRecognizerDelegate {
+    override open func viewDidLoad() {
+        super.viewDidLoad()
+        interactivePopGestureRecognizer?.delegate = self
+    }
+
+    public func gestureRecognizerShouldBegin(_ gestureRecognizer: UIGestureRecognizer) -> Bool {
+        
+        return viewControllers.count > 1
+    }
+}
